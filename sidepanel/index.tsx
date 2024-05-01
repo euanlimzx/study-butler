c8786910b6bf4ac34bee0d79c0fd2bd00f44903c
euanlimzx <109453826+euanlimzx@users.noreply.github.com>
2024-05-01 21:52:18 +0800

    added side panel

--- sidepanel/index.tsx
+++ sidepanel/index.tsx
@@ -1,28 +1,14 @@
+import { ChakraProvider, Flex, Text } from "@chakra-ui/react"
 import { useState } from "react"
 
 function IndexSidePanel() {
-  const [data, setData] = useState("")
-  console.log("hello!")
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <ChakraProvider>
+      <Flex  bg="red" justifyContent="center" flexDirection="column" px="30px">
+        <Text as="span" textAlign="start">Logged in as <Text as="span" fontWeight="bold" textAlign="start">euanlimzx</Text></Text>
+        <Text textAlign="start">Current document selected: Google Docs</Text>
+      </Flex>
+    </ChakraProvider>
   )
 }
 
